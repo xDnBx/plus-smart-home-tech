a7a3d35127adfc2c5ebc72b5a67bef5edba8446e
Oleg Kitaev <oleg.kitaev61@yandex.ru>
2025-05-15 13:46:48 +0300

    Fix remarks after review

--- telemetry/collector/src/main/java/ru/yandex/practicum/service/HubEventService.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/HubEventService.java
@@ -2,10 +2,11 @@ package ru.yandex.practicum.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.kafka.KafkaClient;
+import ru.yandex.practicum.kafka.KafkaProducerService;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.mapper.HubEventMapper;
 import ru.yandex.practicum.model.hub.HubEvent;
@@ -14,16 +15,24 @@ import ru.yandex.practicum.model.hub.HubEvent;
 @Service
 @RequiredArgsConstructor
 public class HubEventService implements EventService<HubEvent> {
-    private final KafkaClient kafkaClient;
+    private final KafkaProducerService kafkaProducer;
     private final HubEventMapper hubEventMapper;
 
-    @Value("${kafkaHubTopic:telemetry.hubs.v1}")
+    @Value("${kafka.hub-topic}")
     private String topic;
 
     @Override
     public void sendEvent(HubEvent event) {
         HubEventAvro hubEventAvro = hubEventMapper.toAvro(event);
-        kafkaClient.getProducer().send(new ProducerRecord<>(topic, hubEventAvro));
+        ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(
+                topic,
+                null,
+                event.getTimestamp().toEpochMilli(),
+                hubEventAvro.getHubId(),
+                hubEventAvro
+        );
+        kafkaProducer.send(record);
+        kafkaProducer.flush();
         log.info("Отправлено событие в топик {}: {}", topic, hubEventAvro);
     }
 }
--- telemetry/serialization/avro-schemas/src/main/java/serializer/AvroSerializer.java
+++ telemetry/serialization/avro-schemas/src/main/java/serializer/AvroSerializer.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.kafka;
+package serializer;
 
 import org.apache.avro.io.BinaryEncoder;
 import org.apache.avro.io.DatumWriter;
