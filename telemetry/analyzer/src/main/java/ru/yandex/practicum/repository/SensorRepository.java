2b80862c5f8688aacc98a209e4623dce06d2dd20
Oleg Kitaev <oleg.kitaev61@yandex.ru>
2025-05-26 21:33:42 +0300

    Fix remarks after review

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/repository/SensorRepository.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/repository/SensorRepository.java
@@ -5,11 +5,9 @@ import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.model.Sensor;
 
 import java.util.Collection;
-import java.util.Optional;
 
 @Repository
 public interface SensorRepository extends JpaRepository<Sensor, String> {
     Boolean existsByIdInAndHubId(Collection<String> ids, String hubId);
-    Optional<Sensor> findByIdAndHubId(String id, String hubId);
     void deleteByIdAndHubId(String id, String hubId);
 }
