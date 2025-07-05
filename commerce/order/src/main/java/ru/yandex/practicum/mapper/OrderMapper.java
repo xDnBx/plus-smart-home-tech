f4b1e48f4b7edee65ca1edb8cac570a5d6103831
Oleg Kitaev <oleg.kitaev61@yandex.ru>
2025-07-05 21:36:37 +0300

    Delete unused method in mappers

--- commerce/order/src/main/java/ru/yandex/practicum/mapper/OrderMapper.java
+++ commerce/order/src/main/java/ru/yandex/practicum/mapper/OrderMapper.java
@@ -1,13 +1,10 @@
 package ru.yandex.practicum.mapper;
 
 import org.mapstruct.Mapper;
-import ru.yandex.practicum.dto.order.CreateNewOrderRequest;
 import ru.yandex.practicum.dto.order.OrderDto;
 import ru.yandex.practicum.model.Order;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
-    Order toEntity(CreateNewOrderRequest request);
-
     OrderDto toDto(Order entity);
 }
--- commerce/payment/src/main/java/ru/yandex/practicum/mapper/PaymentMapper.java
+++ commerce/payment/src/main/java/ru/yandex/practicum/mapper/PaymentMapper.java
@@ -6,7 +6,5 @@ import ru.yandex.practicum.model.Payment;
 
 @Mapper(componentModel = "spring")
 public interface PaymentMapper {
-    Payment toEntity(PaymentDto paymentDto);
-
     PaymentDto toDto(Payment payment);
 }
